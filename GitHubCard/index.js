4a689adb8bedfd2f069e0b8921e659dd2abea8e2
Zach Brown <54756689+zbrown36@users.noreply.github.com>
2020-03-20 22:55:28 -0400

    comp personal card

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,44 @@
            https://api.github.com/users/<your name>
 */
 
+const entryPoint = document.querySelector('.cards');
+
+const handArr = [];
+
+axios.get("https://api.github.com/users/zbrown36")
+
+.then(response => {
+  console.log(response.data)
+  entryPoint.appendChild(gitCard(response.data))
+})
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+
+  });
+
+
+// Make a request for a user with a given ID
+// axios.get("https://api.github.com/users/zbrown36")
+//   .then(function (response) {
+//     // handle success
+//     console.log(response.data);
+//     entryPoint.appendChild(gitCard(response.data))
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+
+//   });
+
+  
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,11 +62,12 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
+  
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -53,3 +92,72 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const gitCard = data => {
+
+  const newCard = document.createElement('div')
+  const gitPic = document.createElement('img')
+  const info = document.createElement('div')
+  const name = document.createElement('h3')
+  const user = document.createElement('p')
+  const location = document.createElement('p')
+  const gitHubPro = document.createElement('p')
+  const link = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  entryPoint.append(newCard)
+  newCard.appendChild(gitPic)
+  newCard.appendChild(info)
+  info.appendChild(name)
+  info.appendChild(user)
+  info.appendChild(location)
+  info.appendChild(gitHubPro)
+  gitHubPro.appendChild(link)
+  info.appendChild(followers)
+  info.appendChild(following)
+  info.appendChild(bio)
+
+  newCard.classList.add('card')
+  info.classList.add('card-info')
+  name.classList.add('name')
+  user.classList.add('username')
+
+  gitPic.src = data.avatar_url
+  name.textContent = data.name
+  user.textContent = data.login
+  location.textContent = `location: ${data.location}`
+  gitHubPro.innerHTML = `Profile: <a href="${data.html_url}"> ${data.html_url}`
+  link.href = "http://github.com/<username>"
+  followers.textContent = `Followers: ${data.followers}`
+  following.textContent = `Following: ${data.following}`
+
+
+  return gitCard;
+
+}
+
+const followersArray = [
+  'TheTrabin',
+  'andrecolon',
+  'mgroff1',
+  'michealengland',
+  'reannalp'
+
+];
+
+followersArray.forEach (element => {
+  axios.get(`https://api.github.com/users/${element}`)
+  then(response => {
+    console.log(response.data)
+    entryPoint.appendChild(gitCard(response.data))
+  })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+  
+    })});
